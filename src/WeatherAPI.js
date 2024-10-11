ff8e6d62b57d370a136ad26eb8daaf01977420ef
marave <marek.vaskovych@seznam.cz>
2024-10-11 19:08:17 +0200

    Skryl jsem soukromý api Key

--- src/WeatherAPI.js
+++ src/WeatherAPI.js
@@ -1,13 +1,20 @@
 class WeatherAPI {
 
+    constructor(apiKey) {
+  
+      this.apiKey = apiKey;
+  
+    }
+    
+
     async fetchWeatherData(city) {
-        const url = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fcbe43ccf4ab713f9a3b640bbd239d3a&units=metric');
+        const url = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric');
         const response = await fetch(url);
         return response.json();
     }
 
     async fetchForecastData(city) {
-        const url = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=fcbe43ccf4ab713f9a3b640bbd239d3a&units=metric');
+        const url = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${this.apiKey}&units=metric');
         const response = await fetch(url);
         return response.json();
     }
