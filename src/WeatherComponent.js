ed906a0fcf0ea0416737c0e81db9d0194de267a8
marave <marek.vaskovych@seznam.cz>
2024-10-12 19:21:21 +0200

    Vložil jsem useEffect do JSX šablony, aplikace nyní plně využívá data z API

--- src/WeatherComponent.js
+++ src/WeatherComponent.js
@@ -4,56 +4,65 @@ import useWeatherAPI from "./useWeatherAPI";
 
 const WeatherComponent = () => {
     const apiKey = 'fcbe43ccf4ab713f9a3b640bbd239d3a';
-    const { weatherData, forecastData, city, searchCity, handleSearchCity, handleSearch } = useWeatherAPI(apiKey);
+    const { weatherData, forecastData, searchCity, handleSearchCity, handleSearch } = useWeatherAPI(apiKey);
+
+    const formatDay = (day) => {
+        return moment(day).format('dddd');
+    }
 
     const formatDate = (date) => {
-        return moment(date).format('dddd, MMMM, Do YYYY');
+        return moment(date).format('DD.MM.YY');
+    }
+    const formatTime = (time) => {
+        return moment(time).format('HH:MM');
     }
 
     return (
         <div className="app">
             <div className="search">
-                <input type="text" value={searchCity} onChange={handleSearchCity} placeholder="Zadejte město" />
+                <input type="text" value={searchCity} onChange={handleSearchCity} placeholder="Zadejte město"></input>
                 <button onClick={handleSearch}>Hledej!</button>
             </div>
 
             <div className='container'>
                 <div className='header'>
                     <div className='location'>
-                        <h2>Prague</h2>
+                        <h2>{weatherData.name}</h2>
                     </div>
                     <div className='temp'>
-                        <h1>23°C</h1>
+                        <h1>{weatherData.main && weatherData.main.temp.toFixed()}°C</h1>
                     </div>
                     <div className='description'>
-                        <p>Oblačno</p>
+                        <p>{weatherData.weather && weatherData.weather[0].main}</p>
                     </div>
                 </div>
                 <div>
                     <div className='forecast'>
-                        <ul >
-
-                            <li className='bottom'>
-                                <h2>Pondělí</h2>
-                                <div className='date'>
-                                </div>
-                                <div className='forecast-temp'>
-                                    <p className='bold'>23°C</p>
-                                    <p>Teplota</p>
-                                </div>
-                                <div className='hummidity'>
-                                    <p className='bold'>90%</p>
-                                    <p>Vlhkost</p>
-                                </div>
-                                <div className='forecast-description'>
-                                    <p className='bold'>Oblačno</p>
-                                    <p>Stav</p>
-                                </div>
-                                <div className='feeling'>
-                                    <p className='bold'>25°C</p>
-                                    <p>Pocitová</p>
-                                </div>
-                            </li>
+                        <ul>
+                            {forecastData.list && forecastData.list.map((forecast, index) => (
+                                <li className='bottom' key={index}>
+                                    <div className='date'>
+                                        <p className="bold">{formatDay(forecast.dt * 1000)}</p>
+                                        <p className="date">{formatDate(forecast.dt * 1000)}</p>
+                                    </div>
+                                    <div className='forecast-temp'>
+                                        <p className='bold'>{forecast.main.temp.toFixed()}°C</p>
+                                        <p>Teplota</p>
+                                    </div>
+                                    <div className='hummidity'>
+                                        <p className='bold'>{forecast.main.humidity}%</p>
+                                        <p>Vlhkost</p>
+                                    </div>
+                                    <div className='forecast-description'>
+                                        <p className='bold'>{forecast.weather[0].main}</p>
+                                        <p>Stav</p>
+                                    </div>
+                                    <div className='feeling'>
+                                        <p className='bold'>{forecast.main.feels_like.toFixed()}°C</p>
+                                        <p>Pocitová</p>
+                                    </div>
+                                </li>
+                            ))};
 
                         </ul>
                     </div>
@@ -63,7 +72,7 @@ const WeatherComponent = () => {
 
         </div>
     );
-}
+};
 
 export default WeatherComponent;
 
--- src/useWeatherAPI.js
+++ src/useWeatherAPI.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { fetch } from 'cross-fetch';
 
 const useWeatherAPI = (apiKey) => {
     const [weatherData, setWeatherData] = useState({});
     const [forecastData, setForecastData] = useState({});
-    const [city, setCity] = useState('Prague');
-    const [searchCity, setSearchCity] = useState(' ');
+    const [city, setCity] = useState('Olomouc');
+    const [searchCity, setSearchCity] = useState('');
 
 
     useEffect(() => {
         const fetchWeatherData = async () => {
-            const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric';
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
             const response = await fetch(url);
             const data = await response.json();
             setWeatherData(data);
         };
 
         const fetchForecastData = async () => {
-            const url = 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${this.apiKey}&units=metric';
+            const url =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
             const response = await fetch(url);
             const data = await response.json();
             setForecastData(data);
